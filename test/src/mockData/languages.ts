c7df8d77f8531ea24fb428330721daac81a5623e
greallra <greallra@tcd.ie>
2024-07-08 09:59:49 +0100

    removed test

--- test/src/mockData/languages.ts
+++
@@ -1,7 +0,0 @@
-export default [
-    { label: 'English', image: images.english },
-    { label: 'German', image: images.german },
-    { label: 'Italian', image: images.italian },
-    { label: 'French', image: images.french },
-    { label: 'Polish', image: images.polish },
-  ];
